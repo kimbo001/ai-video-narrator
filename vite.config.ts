e4470401520c4a2545a7e7dd9a6baccadaea0d0e
kimbo001 <kimboster@gmailcom>
2025-12-16 16:49:40 +0200

    game

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,6 +13,11 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.API_KEY': JSON.stringify(env.VITE_GOOGLE_API_KEY),
       'process.env.API_KEY_PRO': JSON.stringify(env.VITE_GOOGLE_API_KEY_PRO),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
     // dev-server fallback for React-Router
     server: {
